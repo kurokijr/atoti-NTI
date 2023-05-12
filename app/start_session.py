b1411b3974955e9c5812f87953b9d0531690c460
kurokijr <kurokijr@gmail.com>
2023-05-12 07:45:47 -0300

    reverse to DB Connection

--- app/start_session.py
+++ app/start_session.py
@@ -6,7 +6,9 @@ import atoti as tt
 from pydantic import AnyUrl
 
 from .config import Config
-from .create_expenses import create_expenses_table
+from .create_and_join_tables import create_and_join_tables
+from .create_cubes import create_cubes
+from .load_tables import load_tables
 
 
 def create_session(*, config: Config) -> tt.Session:
@@ -25,11 +27,7 @@ def create_session(*, config: Config) -> tt.Session:
 def start_session(*, config: Config) -> tt.Session:
     """Start the session, declare the data model and load the initial data."""
     session = create_session(config=config)
-
-    create_expenses_table(session)
-
-    # create_and_join_tables(session)
-    # create_cubes(session)
-    # load_tables(session, config=config)
-
+    create_and_join_tables(session)
+    create_cubes(session)
+    load_tables(session, config=config)
     return session
